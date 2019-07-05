5b3ef0f628d152e2f513d387d23ff9bca20a64d9
Patrick <Patricknnoonan@gmail.com>
2019-07-05 11:09:38 -0500

    Added basics of d3 bar chart that takes age groups data

--- Capstone/Capstone/wwwroot/js/ProfilePageScripts.js
+++ Capstone/Capstone/wwwroot/js/ProfilePageScripts.js
@@ -115,7 +115,6 @@
     }
 
     function displayPlaces(placeData) {
-        console.log(placeData);
         $(".info-container")
             .append(
                 `<div class="row box" style="padding:10px 0 10px 0">
@@ -199,7 +198,6 @@
                 "Country": poiCountry
             }),
             success: function (data) {
-                console.log(data);
             }
         })
         $(".submitDiv").append("<div class='message success'>Thank you for submitting your review!!</div>")
@@ -225,7 +223,6 @@
         url = "https://en.wikipedia.org/w/api.php?action=query&prop=description&titles=" + item.toString() + "&prop=extracts&exintro&explaintext&format=json&redirects&callback=?";
         $.getJSON(url, function (json) {
             var item_id = Object.keys(json.query.pages)[0];
-            console.log(json);
             sent = json.query.pages[item_id].extract;
             longResult = "<t><strong>" + item + "</strong></t> " + sent;
 
@@ -251,12 +248,28 @@
     }
 
     function attomDataSearch(item) {
+        let fipsArray = [
+            {
+                countyName: "Milwaukee",
+                fips: 55079
+            },
+            {
+                countyName: "Chicago",
+                fips: 17031
+            }
+        ]
+        var itemFips;
 
-        
+        for (let i = 0; i < fipsArray.length; i++) {
+            if (item == fipsArray[i].countyName) {
+                itemFips = fipsArray[i].fips;
+                break;
+            }
+        }
 
         $.ajax({
             method: "Get",
-            url: "https://api.gateway.attomdata.com/communityapi/v2.0.0/area/full?AreaId=CO44003",
+            url: "https://api.gateway.attomdata.com/communityapi/v2.0.0/area/full?AreaId=CO" + itemFips,
             datatype: "JSON",
             headers: {
                 "accept": "application/json",
@@ -264,14 +277,224 @@
             },
             success: function (data) {
                 console.log(data);
+                //get necessary data
+                let extractedData = {
+                    "20_24": data.response.result.package.item[0].age20_24,
+                    "25_29": data.response.result.package.item[0].age25_29,
+                    "30_34": data.response.result.package.item[0].age30_34,
+                    "35_39": data.response.result.package.item[0].age35_39,
+                    "40_44": data.response.result.package.item[0].age40_44,
+                    "45_49": data.response.result.package.item[0].age45_49,
+                    "50_54": data.response.result.package.item[0].age50_54,
+                    "55_59": data.response.result.package.item[0].age55_59,
+                    "60_64": data.response.result.package.item[0].age60_64,
+                    "65_69": data.response.result.package.item[0].age65_69
+                }
+                displayPopulationGraph(extractedData);
+            }
+        });
+    }
+
+    function displayPopulationGraph(attomData) {
+        console.log(attomData);
+        let chartData = [];
+        let chartDates = [];//chart Ages
+
+        for (var key in attomData) {
+            if (attomData.hasOwnProperty(key)) {
+                chartData.push(parseInt(attomData[key]))
+                chartDates.push(key)
+            }
+        }
+
+
+        //let pastSevenDays = [];
+        //let pastThirtyData = checkPastThirty();
+        //let allTimeVisitors = checkAllTime();
+
+        //for (let i = data.length - 7; i < data.length; i++) {
+        //    chartData.push(data[i].count);
+        //}
+
+        //for (let i = 0; i < 7; i++) {
+        //    let d = new Date();
+        //    d.setDate(d.getDate() - i);
+        //    let dString = (d.getMonth() + 1 + "-" + d.getDate());
+        //    pastSevenDays.unshift(dString)
+        //}
+
+        //for (let i = data.length - 7; i < data.length; i++) {
+        //    chartDates.push(data[i].date);
+        //}
+
+        //function checkPastThirty() {
+        //    let pastThirtyArray = [];
+        //    if (data.length < 30) {
+        //        for (let i = 0; i < data.length; i++) {
+        //            pastThirtyArray.push(data[i].count);
+        //        }
+        //    } else {
+        //        for (let i = data.length - 30; i < data.length; i++) {
+        //            pastThirtyArray.push(data[i].count);
+        //        }
+        //    }
+        //    return pastThirtyArray;
+        //}
+
+        //function checkAllTime() {
+        //    let allTimeArray = [];
+        //    for (let i = 0; i < data.length; i++) {
+        //        allTimeArray.push(data[i].count);
+        //    }
+        //    return allTimeArray;
+        //}
+
+        //document.getElementById("past7Total").innerHTML = chartData.reduce(pastSevenTotal);
+        //function pastSevenTotal(total, num) {
+        //    return total + num;
+        //}
+        //document.getElementById("past30Total").innerHTML = pastThirtyData.reduce(pastThirtyTotal);
+        //function pastThirtyTotal(total, num) {
+        //    return total + num;
+        //}
+        //document.getElementById("allTimeTotal").innerHTML = allTimeVisitors.reduce(allTimeTotal);
+        //function allTimeTotal(total, num) {
+        //    return total + num;
+        //}
+
+        //----------------------------------------Bar Chart-----------------------------
+
+        function BarChart() {
+
+            let margin = {
+                top: 30,
+                right: 10,
+                bottom: 30,
+                left: 50
             }
 
-           
+            let height = 300 - margin.top - margin.bottom;
+            let width = 620 - margin.left - margin.right;
 
-        });
+            let dynamicColor;
 
+            let yScale = d3.scaleLinear()
+                .domain([0, d3.max(chartData)])
+                .range([0, height])
+
+            let xScale = d3.scaleBand()
+                .domain(d3.range(0, 7))
+                .range([0, width])
+
+            let colors = d3.scaleLinear()
+                .domain([0, chartData.length * .33, chartData.length * .66, chartData.length])
+                .range(['#d6e9c6', '#bce8f1', '#faebcc', '#ebccd1'])
+
+            let awesome = d3.select('#bar-chart').append('svg')
+                .attr('width', width + margin.left + margin.right)
+                .attr('height', height + margin.top + margin.bottom)
+
+                .append('g')
+                .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+                .selectAll('rect').data(chartData)
+                .enter().append('rect')
+                .styles({
+                    'fill': function (data, i) {
+                        return colors(i);
+                    },
+                    'stroke': '#31708f',
+                    'stroke-width': '1'
+                })
+
+                .attr('width', xScale.bandwidth())
+                .attr('x', function (data, i) {
+                    return xScale(i);
+                })
+
+                .attr('height', 0)
+                .attr('y', height)
+                //.append('svg:title')
+                //        .text(function (data, i) { return i.count; })
+                //        .text("hello")
+
+                .on('mouseover', function (data) {
+                    dynamicColor = this.style.fill;
+                    d3.select(this)
+                        .style('fill', '#3c763d')
+
+                })
+
+                .on('mouseout', function (data) {
+                    d3.select(this)
+                        .style('fill', dynamicColor)
+                    $('svg.title').remove();
+                })
+
+
+            awesome.transition()
+                .attr('height', function (data) {
+                    return yScale(data);
+                })
+                .attr('y', function (data) {
+                    return height - yScale(data);
+                })
+                .delay(function (data, i) {
+                    return i * 20;
+                })
+
+                .duration(2000)
+                .ease(d3.easeElastic)
+            let verticalGuideScale = d3.scaleLinear()
+                .domain([0, d3.max(chartData)])
+                .range([height, 0])
+
+            let vAxis = d3.axisLeft(verticalGuideScale)
+                .ticks()
+
+            let verticalGuide = d3.select('svg').append('g')
+            vAxis(verticalGuide)
+            verticalGuide.attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+            verticalGuide.selectAll('path')
+                .styles({
+                    fill: 'none',
+                    stroke: "#3c763d"
+                })
+
+            verticalGuide.selectAll('line')
+                .styles({
+                    stroke: "#3c763d"
+                })
+
+            let horizontalGuideScale = d3.scaleLinear()
+                .domain([0, d3.max(chartDates)])
+                .range([width, 0])
+
+            let hAxis = d3.axisBottom(xScale)
+                .tickFormat(function (d, i) {
+                    return chartDates[i];//------------------------------------
+                })
+
+            let horizontalGuide = d3.select('svg').append('g')
+
+            hAxis(horizontalGuide)
+            horizontalGuide.attr('transform', 'translate(' + margin.left + ', ' + (height + margin.top) + ')')
+            horizontalGuide.selectAll('path')
+                .styles({
+                    fill: 'none',
+                    stroke: "#3c763d"
+                })
+            horizontalGuide.selectAll('line')
+                .styles({
+                    stroke: "#3c763d"
+                });
+        }
+        BarChart();
     }
 });
+
+
+
+
 
 var scrollingElement = (document.scrollingElement || document.body || body);
 
